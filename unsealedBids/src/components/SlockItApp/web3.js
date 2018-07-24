fe28f819e571472d7fa8588ec2063d2522521a67
gvsrusa <gvsrusa@gmail.com>
2018-07-24 12:24:27 -0600

    updated to mainent API

--- unsealedBids/src/components/SlockItApp/web3.js
+++ unsealedBids/src/components/SlockItApp/web3.js
@@ -8,7 +8,7 @@ if (typeof window !== 'undefined' && typeof window.web3 !== 'undefined') {
 } else {
   // We are on the server *OR* the user is not running metamask
   const provider = new Web3.providers.HttpProvider(
-    'https://rinkeby.infura.io/orDImgKRzwNrVCDrAk5Q' // rinkeby is only for testing purpose
+    'https://mainnet.infura.io/orDImgKRzwNrVCDrAk5Q' // Mainnet API provided by infura
   );
   web3 = new Web3(provider);
 }
